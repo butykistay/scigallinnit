d26c90e0f9b57b6410baf84cd32f0a2106c963ad
butykistay <imrani27@uwcea.org>
2024-12-27 18:23:32 +0300

    Add files via upload
    yeah eyah eyeah

---
+++ MyCode/scripts/app.js
@@ -0,0 +1,7 @@
+// Example: Display a message on project click
+document.querySelectorAll('.project-card').forEach(card => {
+    card.addEventListener('click', () => {
+      alert('You clicked on a project!');
+    });
+  });
+  
